64459d053a858dcede0571f872c06685c02cc92f
miampf <miampf@proton.me>
2024-06-03 19:53:37 +0200

    working flake

--- template/flake.nix
+++ template/flake.nix
@@ -1,19 +1,22 @@
 {
-  description = "Flake utils demo";
+  description = "Gleative flake setup.";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in
+      with pkgs;
       {
-        packages.default = rec {
-          buildInputs = [ gleam deno ];
+        packages.gleative-build = pkgs.writeScriptBin "gleative-build" ''
+        gleam run -m gleative
+        '';
 
-          buildPhase = ''
-          gleam run -m gleative
-          '';
-        }
+        apps.build = {
+          type = "app";
+          buildInputs = [ gleam deno ];
+          program = "${self.packages.${system}.gleative-build}/bin/gleative-build";
+        };
       }
     );
 }
